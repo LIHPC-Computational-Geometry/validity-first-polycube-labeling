71ed6a4fbc60a2f60d79ffc08d54b6b8754509a4
Sebastien Mestrallet <sebastien.mestrallet@hey.com>
2023-07-24 16:29:43 +0200

    add auto fix monotonicity

--- app/automatic_polycube.cpp
+++ app/automatic_polycube.cpp
@@ -42,11 +42,11 @@ private:
 				update_static_labeling_graph(allow_boundaries_between_opposite_labels_);
 			}
 
-			if(ImGui::Button("Auto fix")) {
-				if(auto_fix(100)) {
-					fmt::println(Logger::out("fix_labeling"),"auto fix found a valid labeling"); Logger::out("fix_labeling").flush();
+			if(ImGui::Button("Auto fix validity")) {
+				if(auto_fix_validity(100)) {
+					fmt::println(Logger::out("fix_labeling"),"auto fix validity found a valid labeling"); Logger::out("fix_labeling").flush();
 				}
-				// else : auto_fix() already printed a message
+				// else : auto_fix_validity() already printed a message
 			}
 
 			ImGui::Separator();
@@ -58,11 +58,18 @@ private:
 				fmt::println(Logger::out("monotonicity"),"label of {} facets modified",nb_facets_modified); Logger::out("monotonicity").flush();
 				update_static_labeling_graph(allow_boundaries_between_opposite_labels_);
 			}
+
+			if(ImGui::Button("Auto fix monotonicity")) {
+				if(auto_fix_monotonicity(500)) {
+					fmt::println(Logger::out("fix_labeling"),"auto fix monotonicity found a labeling with all-monotone boundaries"); Logger::out("fix_labeling").flush();
+				}
+				// else : auto_fix_monotonicity() already printed a message
+			}
 		}
 	}
 
 	// return true if successfully found a valid labeling
-	bool auto_fix(unsigned int max_nb_loop) {
+	bool auto_fix_validity(unsigned int max_nb_loop) {
 		unsigned int nb_loops = 0;
 		unsigned int nb_fixed_features = 0;
 		std::set<std::array<std::size_t,7>> set_of_labeling_features_combinations_encountered;
@@ -130,10 +137,27 @@ private:
 		}
 
 		if(!static_labeling_graph.is_valid()) {
-			fmt::println(Logger::out("fix_labeling"),"auto fix stopped (max nb loops reached), no valid labeling found"); Logger::out("fix_labeling").flush();
+			fmt::println(Logger::out("fix_labeling"),"auto fix validity stopped (max nb loops reached), no valid labeling found"); Logger::out("fix_labeling").flush();
 			return false;
 		}
 		
+		return true;
+	}
+
+	bool auto_fix_monotonicity(unsigned int max_nb_steps) {
+		unsigned int nb_steps = 0;
+		while(!static_labeling_graph.non_monotone_boundaries.empty() && nb_steps <= max_nb_steps) {
+			move_boundaries_near_turning_points(mesh_,LABELING_ATTRIBUTE_NAME,static_labeling_graph);
+			update_static_labeling_graph(allow_boundaries_between_opposite_labels_);
+			remove_surrounded_charts(mesh_,LABELING_ATTRIBUTE_NAME,static_labeling_graph);
+			update_static_labeling_graph(allow_boundaries_between_opposite_labels_);
+		}
+
+		if(!static_labeling_graph.non_monotone_boundaries.empty()) {
+			fmt::println(Logger::out("fix_labeling"),"auto fix monotonicity stopped (max nb steps reached), it remains non-monotone boundaries"); Logger::out("fix_labeling").flush();
+			return false;
+		}
+
 		return true;
 	}
 };
