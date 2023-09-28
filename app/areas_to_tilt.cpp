cd93ffb7e7a1ec1d0f12ca620e60283f8c5fde6c
Sébastien Mestrallet <sebastien.mestrallet@hey.com>
2023-09-28 14:22:55 +0200

    `areas_to_tilt` groups facets by area

--- app/areas_to_tilt.cpp
+++ app/areas_to_tilt.cpp
@@ -1,12 +1,16 @@
 #include <geogram/mesh/mesh_geometry.h>
 #include <geogram/basic/command_line.h>
 #include <geogram/basic/logger.h>
+#include <geogram/basic/vecg.h>
 
 #include <fmt/core.h>
 #include <fmt/ostream.h>
 
+#include <DisjointSet.hpp>
+
 #include <vector>
 #include <array>
+#include <map>
 #include <utility> // for std::pair<>
 #include <algorithm>
 
@@ -24,13 +28,19 @@ public:
         show_volume_ = false;
         show_colored_cells_ = false;
         // init own variables
-        sensitivity = 0.001f;
+        sensitivity_ = 0.3f; // 0.02f
+        group_by_area_ = false;
+        nb_areas = 0;
     }
 
 protected:
 
     void draw_object_properties() override {
-        ImGui::SliderFloat("Sensitivity",&sensitivity,0.0f,1.0f,"%0.4f");
+        ImGui::SliderFloat("Sensitivity",&sensitivity_,0.0f,1.0f,"%0.4f");
+        if(ImGui::Checkbox("Group by area",&group_by_area_)) {
+            update_drawing_settings();
+        }
+        ImGui::Text("nb_areas=%lu",nb_areas);
         if(ImGui::Button("Apply")) {
             compute_areas_to_tilt();
         }
@@ -52,14 +62,14 @@ protected:
         mesh_.vertices.set_double_precision();
 
         // compute facet normals
-        normals.resize(mesh_.facets.nb());
+        normals_.resize(mesh_.facets.nb());
         FOR(f,mesh_.facets.nb()) { // for each facet
-            normals[f] = normalize(Geom::mesh_facet_normal(mesh_,f));
+            normals_[f] = normalize(Geom::mesh_facet_normal(mesh_,f));
         }
 
         compute_areas_to_tilt();
 
-        // dont choose a colormap now because GL is not likely to be initialized
+        // don't call update_drawing_settings() now because GL is not likely to be initialized
 
         return true;
     }
@@ -67,14 +77,7 @@ protected:
     void GL_initialize() override {
 		SimpleMeshApplicationExt::GL_initialize();
 		if(mesh_.vertices.nb()) { // if a mesh has been loaded
-			lighting_ = false;
-            show_attributes_ = true;
-            current_colormap_texture_ = colormaps_[COLORMAP_BLUE_RED].texture;
-            attribute_min_ = 0.0f;
-            attribute_max_ = 1.0f;
-            attribute_ = "facets.on_area_to_tilt";
-            attribute_name_ = "on_area_to_tilt";
-            attribute_subelements_ = MESH_FACETS;
+			update_drawing_settings();
 		}
 	}
 
@@ -83,7 +86,7 @@ protected:
         Attribute<bool> on_area_to_tilt(mesh_.facets.attributes(),"on_area_to_tilt");
         unsigned int nb_facet_on_areas_to_tilt = 0;
         FOR(f,mesh_.facets.nb()) { // for each facet
-            const vec3& normal = normals[f];
+            const vec3& normal = normals_[f];
             // based on labeling.cpp nearest_label()
             std::array<std::pair<double,index_t>,6> per_label_weights = {
                 std::make_pair(normal.x < 0.0 ? 0.0 : normal.x,     0), // +X
@@ -94,7 +97,7 @@ protected:
                 std::make_pair(normal.z < 0.0 ? -normal.z : 0.0,    5)  // -Z
             };
             std::sort(per_label_weights.begin(),per_label_weights.end());
-            if(std::abs(per_label_weights[5].first-per_label_weights[4].first) < (double) sensitivity) {
+            if(std::abs(per_label_weights[5].first-per_label_weights[4].first) < (double) sensitivity_) {
                 // the 2 labels with the most weight are too close
                 nb_facet_on_areas_to_tilt++;
                 on_area_to_tilt[f] = true;
@@ -105,10 +108,51 @@ protected:
         }
 
         fmt::println(Logger::out("areas to tilt"),"{} facet(s) over {} are on an area to tilt",nb_facet_on_areas_to_tilt,mesh_.facets.nb()); Logger::out("areas to tilt").flush();
+
+        DisjointSet<index_t> ds(mesh_.facets.nb());
+        index_t neighbor_facet = index_t(-1);
+        FOR(current_facet,mesh_.facets.nb()) {
+            FOR(le,3) { // for each of the 3 local edges
+                neighbor_facet = mesh_.facets.adjacent(current_facet,le);
+                if(on_area_to_tilt[current_facet] == on_area_to_tilt[neighbor_facet]) {
+                    ds.mergeSets(current_facet,neighbor_facet);
+                }
+            }
+        }
+
+        Attribute<index_t> area_index(mesh_.facets.attributes(),"area_index");
+        nb_areas = ds.getSetsMap(area_index.data());
+
+        fmt::println(Logger::out("areas to tilt"),"facets grouped into {} area(s)",nb_areas); Logger::out("areas to tilt").flush();
     }
 
-    std::vector<vec3> normals; // per facet normals
-    float sensitivity;
+    void update_drawing_settings() {
+        if(group_by_area_) {
+            lighting_ = false;
+            show_attributes_ = true;
+            current_colormap_texture_ = colormaps_[COLORMAP_CEI_60757].texture;
+            attribute_min_ = 0.0f;
+            attribute_max_ = (float) nb_areas+1;
+            attribute_ = "facets.area_index";
+            attribute_name_ = "area_index";
+            attribute_subelements_ = MESH_FACETS;
+        }
+        else {
+            lighting_ = false;
+            show_attributes_ = true;
+            current_colormap_texture_ = colormaps_[COLORMAP_BLUE_RED].texture;
+            attribute_min_ = 0.0f;
+            attribute_max_ = 1.0f;
+            attribute_ = "facets.on_area_to_tilt";
+            attribute_name_ = "on_area_to_tilt";
+            attribute_subelements_ = MESH_FACETS;
+        }
+    }
+
+    std::vector<vec3> normals_; // per facet normals
+    float sensitivity_;
+    bool group_by_area_;
+    std::size_t nb_areas;
 
 };
 
