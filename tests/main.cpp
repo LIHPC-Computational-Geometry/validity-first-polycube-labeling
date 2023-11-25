db3420309345e84696f7edb862949cd1177ab7f3
Sébastien Mestrallet <sebastien.mestrallet@hey.com>
2023-11-25 12:07:39 +0100

    create cube mesh in tests executable to test halfedge operators WIP

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,10 +3,21 @@
 #include <array>
 #include <random>
 
-#include "basic_stats.h"    // for BasicStats
-#include "containers.h"     // for std_dev()
+#include <geogram/mesh/mesh.h>                  // for Mesh
+#include <geogram/basic/vecg.h>                 // for vec3
+#include <geogram/mesh/mesh_io.h>               // for mesh_save()
+#include <geogram/basic/command_line.h>         // for CmdLine::initialize()
+#include <geogram/basic/command_line_args.h>	// for CmdLine::import_arg_group()
+#include <geogram/basic/file_system.h>          // for FileSystem::initialize()
+#include <geogram/mesh/mesh_halfedges.h>        // for MeshHalfedges::Halfedge
+
+#include "basic_stats.h"            // for BasicStats
+#include "containers.h"             // for std_dev()
+#include "CustomMeshHalfedges.h"    // CustomMeshHalfedges
 
 #define DOUBLE_MAX_ABS_ERROR 10e5
+
+using namespace GEO;
 
 TEST(BasicStats, HardCoded) {
     BasicStats stats;
@@ -46,6 +57,158 @@ TEST(BasicStats, Random_1000_sd) { // test the standard deviation computation on
     }
     // compare standard deviation computation from all the values, and the iterative computation
     EXPECT_NEAR(std_dev(values.begin(),values.end()), stats.sd(), DOUBLE_MAX_ABS_ERROR);
+}
+
+// Halfedge operators tests
+// http://google.github.io/googletest/primer.html#same-data-multiple-tests
+class HalfedgesTest : public testing::Test {
+public:
+
+    HalfedgesTest() : testing::Test(), mesh_halfedges(cube) {
+    }; // link the halfedges API to the cube mesh
+
+protected:
+    void SetUp() override {
+
+        // silent Geogram initialization
+        // copy of what's inside GEO::initialize(), modified to have the logger in the "minimal" mode
+        Environment* env = Environment::instance();
+        env->set_value("version", "inaccessible"); // some code after expects the "version" environment variable to exist
+        env->set_value("release_date", "inaccessible"); // idem
+        env->set_value("SVN revision", "inaccessible"); // idem
+        FileSystem::initialize();
+        Logger::initialize();
+        Logger::instance()->set_minimal(true);
+        CmdLine::initialize();
+        CmdLine::import_arg_group("sys"); // declares sys:compression_level, needed by mesh_save() for .geogram files
+        mesh_io_initialize();
+
+        // Create a simple triangle mesh of a cube
+
+        // create the 8 vertices of the cube
+        // Vertex ordering from the Geogram convention for hexahedra, see include/hex_mesh.h
+        //          4-------6   v0 = (0,0,1)
+        //         /|      /|   v1 = (0,0,0)
+        //        / |     / |   v2 = (1,0,1)
+        //       0-------2  |   v3 = (1,0,0)
+        //       |  5----|--7   v4 = (0,1,1)
+        // Z     | /     | /    v5 = (0,1,0)
+        // ^  Y  |/      |/     v6 = (1,1,1)
+        // | /   1-------3      v7 = (1,1,0)
+        // |/
+        // o----> X
+        index_t index_of_first_vertex = cube.vertices.create_vertices(8);
+        geo_assert(index_of_first_vertex == 0);
+        cube.vertices.point(0) = GEO::vec3(0.0, 0.0, 1.0);
+        cube.vertices.point(1) = GEO::vec3(0.0, 0.0, 0.0);
+        cube.vertices.point(2) = GEO::vec3(1.0, 0.0, 1.0);
+        cube.vertices.point(3) = GEO::vec3(1.0, 0.0, 0.0);
+        cube.vertices.point(4) = GEO::vec3(0.0, 1.0, 1.0);
+        cube.vertices.point(5) = GEO::vec3(0.0, 1.0, 0.0);
+        cube.vertices.point(6) = GEO::vec3(1.0, 1.0, 1.0);
+        cube.vertices.point(7) = GEO::vec3(1.0, 1.0, 0.0);
+
+        // create the 6*2 triangle facets
+        // inside a given facet, local vertices will be ordered to have an outgoing normal (right hand rule -> counterclockwise)
+        // starting with the vertex of smallest index
+        //
+        // 0-------2    0  0-------2  
+        // |       |    | \  \  f1 |  f0 = vertices 0,1,3
+        // | front | => |   \  \   |
+        // |       |    | f0  \  \ |  f1 = vertices 0,3,2
+        // 1-------3    1-------3  3
+        //
+        // 4-------0    4  4-------0  
+        // |       |    | \  \  f3 |  f2 = vertices 1,4,5
+        // | left  | => |   \  \   |
+        // |       |    | f2  \  \ |  f3 = vertices 0,4,1
+        // 5-------1    5-------1  1
+        //
+        // 6-------4    6  6-------4  
+        // |       |    | \  \  f5 |  f4 = vertices 5,6,7
+        // | back  | => |   \  \   |
+        // |       |    | f4  \  \ |  f5 = vertices 4,6,5
+        // 7-------5    7-------5  5
+        //
+        // 2-------6    2  2-------6  
+        // |       |    | \  \  f7 |  f6 = vertices 2,3,7
+        // | right | => |   \  \   |
+        // |       |    | f6  \  \ |  f7 = vertices 2,7,6
+        // 3-------7    3-------7  7
+        //
+        // 4-------6    4  4-------6  
+        // |       |    | \  \  f9 |  f8 = vertices 0,2,4
+        // |  top  | => |   \  \   |
+        // |       |    | f8  \  \ |  f9 = vertices 2,6,4
+        // 0-------2    0-------2  2
+        //
+        // 1-------3    1  1-------3  
+        // |       |    | \  \  f11|  f10 = vertices 1,5,7
+        // |bottom | => |   \  \   |
+        // |       |    |f10  \  \ |  f11 = vertices 1,7,3
+        // 5-------7    5-------7  7
+        //
+        index_t index_of_first_facet = cube.facets.create_triangles(12);
+        geo_assert(index_of_first_facet == 0);
+        // facet 0
+        cube.facets.set_vertex(0,0,0);
+        cube.facets.set_vertex(0,1,1);
+        cube.facets.set_vertex(0,2,3);
+        // facet 1
+        cube.facets.set_vertex(1,0,0);
+        cube.facets.set_vertex(1,1,3);
+        cube.facets.set_vertex(1,2,2);
+        // facet 2
+        cube.facets.set_vertex(2,0,1);
+        cube.facets.set_vertex(2,1,4);
+        cube.facets.set_vertex(2,2,5);
+        // facet 3
+        cube.facets.set_vertex(3,0,0);
+        cube.facets.set_vertex(3,1,4);
+        cube.facets.set_vertex(3,2,1);
+        // facet 4
+        cube.facets.set_vertex(4,0,5);
+        cube.facets.set_vertex(4,1,6);
+        cube.facets.set_vertex(4,2,7);
+        // facet 5
+        cube.facets.set_vertex(5,0,4);
+        cube.facets.set_vertex(5,1,6);
+        cube.facets.set_vertex(5,2,5);
+        // facet 6
+        cube.facets.set_vertex(6,0,2);
+        cube.facets.set_vertex(6,1,3);
+        cube.facets.set_vertex(6,2,7);
+        // facet 7
+        cube.facets.set_vertex(7,0,2);
+        cube.facets.set_vertex(7,1,7);
+        cube.facets.set_vertex(7,2,6);
+        // facet 8
+        cube.facets.set_vertex(8,0,0);
+        cube.facets.set_vertex(8,1,2);
+        cube.facets.set_vertex(8,2,4);
+        // facet 9
+        cube.facets.set_vertex(9,0,2);
+        cube.facets.set_vertex(9,1,6);
+        cube.facets.set_vertex(9,2,4);
+        // facet 10
+        cube.facets.set_vertex(10,0,1);
+        cube.facets.set_vertex(10,1,5);
+        cube.facets.set_vertex(10,2,7);
+        // facet 11
+        cube.facets.set_vertex(11,0,1);
+        cube.facets.set_vertex(11,1,7);
+        cube.facets.set_vertex(11,2,3);
+    }
+
+    Mesh cube;
+    CustomMeshHalfedges mesh_halfedges;
+    MeshHalfedges::Halfedge init_halfedge; // TODO initialize in SetUp()
+};
+
+TEST_F(HalfedgesTest, ExportCubeMesh) {
+    EXPECT_TRUE(GEO::mesh_save(cube,"cube.obj"));
+    // .geogram export doesn't seem to work properly
+    // Or graphite doesn't properly render .geogram files with few elements
 }
 
 int main(int argc, char** argv)
