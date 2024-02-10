204b048ae77e3964c538d6f49af454384ee69f49
Sébastien Mestrallet <sebastien.mestrallet@hey.com>
2024-02-10 20:41:35 +0100

    swap coordinates and triangles in buffer in write_glTF__triangle_mesh()

--- src/glTF.cpp
+++ src/glTF.cpp
@@ -86,31 +86,31 @@ void write_glTF__triangle_mesh(std::string filename, GEO::Mesh& M) {
     const size_t BUFFER_0 = 0;
     tinygltf::Buffer& buffer_0 = m.buffers[BUFFER_0];
 
-    // resize buffer to 4 bytes * 3 indices * #triangles
-    //                + 4 bytes * 3 floating points * #vertices
+    // resize buffer to 4 bytes * 3 floating points * #vertices
+    //                + 4 bytes * 3 indices * #triangles
     //                + 4 bytes * 2 indices * #edges
-    size_t buffer_indices_start = 0; // byte index
-    size_t buffer_indices_length = 4*3*M.facets.nb(); // number of bytes
-    size_t buffer_coordinates_start = buffer_indices_length; // byte index. just after the indices chunk, no padding
-    size_t buffer_coordinates_length = 4*3*M.vertices.nb(); // number of bytes
-    size_t buffer_edges_start = buffer_coordinates_start+buffer_coordinates_length; // byte index
-    size_t buffer_edges_length = 4*2*edges.size(); // number of bytes
-    buffer_0.data.resize(buffer_indices_length+buffer_coordinates_length+buffer_edges_length);
+    size_t buffer_coordinates_start     = 0;                                                // byte index
+    size_t buffer_coordinates_length    = 4*3*M.vertices.nb();                              // number of bytes
+    size_t buffer_triangles_start       = buffer_coordinates_length;                        // byte index
+    size_t buffer_triangles_length      = 4*3*M.facets.nb();                                // number of bytes
+    size_t buffer_edges_start           = buffer_triangles_start+buffer_triangles_length;   // byte index
+    size_t buffer_edges_length          = 4*2*edges.size();                                 // number of bytes
+    buffer_0.data.resize(buffer_coordinates_length+buffer_triangles_length+buffer_edges_length);
 
-    // write triangles (= vertex indices)
-    FOR(f,M.facets.nb()) { // for each facet (triangle) index
-        memcpy(buffer_0.data.data() + f*12 + 0, facet_vertex_index_ptr(M,f,0),4);
-        memcpy(buffer_0.data.data() + f*12 + 4, facet_vertex_index_ptr(M,f,1),4);
-        memcpy(buffer_0.data.data() + f*12 + 8, facet_vertex_index_ptr(M,f,2),4);
-    }
     // write vertices (= 3D coordinates)
     FOR(v,M.vertices.nb()) { // for each vertex index
-        memcpy(buffer_0.data.data()+buffer_coordinates_start+v*12+0,static_cast<void*>(M.vertices.single_precision_point_ptr(v)),12); // write x,y,z from float* getter
+        memcpy(buffer_0.data.data() + v*12, static_cast<void*>(M.vertices.single_precision_point_ptr(v)), 12); // write x,y,z from float* getter
+    }
+    // write triangles (= vertex indices)
+    FOR(f,M.facets.nb()) { // for each facet (triangle) index
+        memcpy(buffer_0.data.data() + buffer_triangles_start + f*12 + 0, facet_vertex_index_ptr(M,f,0),4);
+        memcpy(buffer_0.data.data() + buffer_triangles_start + f*12 + 4, facet_vertex_index_ptr(M,f,1),4);
+        memcpy(buffer_0.data.data() + buffer_triangles_start + f*12 + 8, facet_vertex_index_ptr(M,f,2),4);
     }
     // write edges (= vertex indices)
     FOR(e,edges.size()) {
-        memcpy(buffer_0.data.data()+buffer_edges_start + 8*e + 0, &(*edges[e].begin()), 4);
-        memcpy(buffer_0.data.data()+buffer_edges_start + 8*e + 4, &(*edges[e].rbegin()), 4);
+        memcpy(buffer_0.data.data() + buffer_edges_start + 8*e + 0, &(*edges[e].begin()),  4);
+        memcpy(buffer_0.data.data() + buffer_edges_start + 8*e + 4, &(*edges[e].rbegin()), 4);
     }    
 
     //////////////////////////
@@ -118,26 +118,26 @@ void write_glTF__triangle_mesh(std::string filename, GEO::Mesh& M) {
     //////////////////////////
 
     m.bufferViews.resize(3);
-    const size_t BUFFERVIEW_0_TRIANGLES_VERTICES = 0;
-    const size_t BUFFERVIEW_1_VERTICES_COORDINATES = 1;
+    const size_t BUFFERVIEW_0_VERTICES_COORDINATES = 0;
+    const size_t BUFFERVIEW_1_TRIANGLES_VERTICES = 1;
     const size_t BUFFERVIEW_2_EDGES_VERTICES = 2;
-    tinygltf::BufferView& bufferview_0_triangles_vertices = m.bufferViews[BUFFERVIEW_0_TRIANGLES_VERTICES];
-    tinygltf::BufferView& bufferview_1_vertices_coordinates = m.bufferViews[BUFFERVIEW_1_VERTICES_COORDINATES];
+    tinygltf::BufferView& bufferview_0_vertices_coordinates = m.bufferViews[BUFFERVIEW_0_VERTICES_COORDINATES];
+    tinygltf::BufferView& bufferview_1_triangles_vertices = m.bufferViews[BUFFERVIEW_1_TRIANGLES_VERTICES];
     tinygltf::BufferView& bufferview_2_edges_vertices = m.bufferViews[BUFFERVIEW_2_EDGES_VERTICES];
 
-    // 1st chunk of the buffer 0 : triangles = vertex indices (type ELEMENT_ARRAY_BUFFER)
-    bufferview_0_triangles_vertices.buffer = BUFFER_0;
-    bufferview_0_triangles_vertices.byteOffset = buffer_indices_start;
-    bufferview_0_triangles_vertices.byteLength = buffer_indices_length;
-    bufferview_0_triangles_vertices.target = TINYGLTF_TARGET_ELEMENT_ARRAY_BUFFER;
+    // 1st chunk of the buffer 0 : points = 3D coordinates (type ARRAY_BUFFER)
+    bufferview_0_vertices_coordinates.buffer = BUFFER_0;
+    bufferview_0_vertices_coordinates.byteOffset = buffer_coordinates_start;
+    bufferview_0_vertices_coordinates.byteLength = buffer_coordinates_length;
+    bufferview_0_vertices_coordinates.target = TINYGLTF_TARGET_ARRAY_BUFFER;
 
-    // 2nd chunk of the bufffer 0 : points = 3D coordinates (type ARRAY_BUFFER)
-    bufferview_1_vertices_coordinates.buffer = BUFFER_0;
-    bufferview_1_vertices_coordinates.byteOffset = buffer_coordinates_start;
-    bufferview_1_vertices_coordinates.byteLength = buffer_coordinates_length;
-    bufferview_1_vertices_coordinates.target = TINYGLTF_TARGET_ARRAY_BUFFER;
+    // 2nd chunk of the bufffer 0 : triangles = vertex indices (type ELEMENT_ARRAY_BUFFER)
+    bufferview_1_triangles_vertices.buffer = BUFFER_0;
+    bufferview_1_triangles_vertices.byteOffset = buffer_triangles_start;
+    bufferview_1_triangles_vertices.byteLength = buffer_triangles_length;
+    bufferview_1_triangles_vertices.target = TINYGLTF_TARGET_ELEMENT_ARRAY_BUFFER;
 
-    // 3rd chunk of the buffer 0 : indices of edge vertices (type ELEMENT_ARRAY_BUFFER)
+    // 3rd chunk of the buffer 0 : edges = vertex indices (type ELEMENT_ARRAY_BUFFER)
     bufferview_2_edges_vertices.buffer = BUFFER_0;
     bufferview_2_edges_vertices.byteOffset = buffer_edges_start;
     bufferview_2_edges_vertices.byteLength = buffer_edges_length;
@@ -148,32 +148,31 @@ void write_glTF__triangle_mesh(std::string filename, GEO::Mesh& M) {
     ///////////////////////
 
     m.accessors.resize(3);
-    const size_t ACCESSOR_0_TRIANGLES_VERTICES = 0;
-    const size_t ACCESSOR_1_VERTICES_COORDINATES = 1;
+    const size_t ACCESSOR_0_VERTICES_COORDINATES = 0;
+    const size_t ACCESSOR_1_TRIANGLES_VERTICES = 1;
     const size_t ACCESSOR_2_EDGES_VERTICES = 2;
-    tinygltf::Accessor& accessor_0_triangles_vertices = m.accessors[ACCESSOR_0_TRIANGLES_VERTICES];
-    tinygltf::Accessor& accessor_1_vertices_coordinates = m.accessors[ACCESSOR_1_VERTICES_COORDINATES];
+    tinygltf::Accessor& accessor_0_vertices_coordinates = m.accessors[ACCESSOR_0_VERTICES_COORDINATES];
+    tinygltf::Accessor& accessor_1_triangles_vertices = m.accessors[ACCESSOR_1_TRIANGLES_VERTICES];
     tinygltf::Accessor& accessor_2_edges_vertices = m.accessors[ACCESSOR_2_EDGES_VERTICES];
 
     // layout description of buffer view 0
-    accessor_0_triangles_vertices.bufferView = BUFFERVIEW_0_TRIANGLES_VERTICES;
-    accessor_0_triangles_vertices.byteOffset = 0;
-    accessor_0_triangles_vertices.componentType = TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT; // 32 bits per index
-    accessor_0_triangles_vertices.count = M.facets.nb()*3; // how many indices
-    accessor_0_triangles_vertices.type = TINYGLTF_TYPE_SCALAR;
-    // range of indices : [ 0 : M.facets.nb()*3-1 ]
-    accessor_0_triangles_vertices.maxValues.push_back(M.vertices.nb()-1); // max value of all indices in the buffer view
-    accessor_0_triangles_vertices.minValues.push_back(0);
+    accessor_0_vertices_coordinates.bufferView = BUFFERVIEW_0_VERTICES_COORDINATES;
+    accessor_0_vertices_coordinates.byteOffset = 0;
+    accessor_0_vertices_coordinates.componentType = TINYGLTF_COMPONENT_TYPE_FLOAT;
+    accessor_0_vertices_coordinates.count = M.vertices.nb(); // how many points
+    accessor_0_vertices_coordinates.type = TINYGLTF_TYPE_VEC3;
+    accessor_0_vertices_coordinates.maxValues = bounding_box_max; // = [max_x, max_y, max_z]
+    accessor_0_vertices_coordinates.minValues = bounding_box_min; // = [min_x, min_y, min_z]
 
     // layout description of buffer view 1
-    accessor_1_vertices_coordinates.bufferView = BUFFERVIEW_1_VERTICES_COORDINATES;
-    accessor_1_vertices_coordinates.byteOffset = 0;
-    accessor_1_vertices_coordinates.componentType = TINYGLTF_COMPONENT_TYPE_FLOAT;
-    accessor_1_vertices_coordinates.count = M.vertices.nb(); // how many points
-    accessor_1_vertices_coordinates.type = TINYGLTF_TYPE_VEC3;
-    // range of coordinate values
-    accessor_1_vertices_coordinates.maxValues = bounding_box_max;
-    accessor_1_vertices_coordinates.minValues = bounding_box_min;
+    accessor_1_triangles_vertices.bufferView = BUFFERVIEW_1_TRIANGLES_VERTICES;
+    accessor_1_triangles_vertices.byteOffset = 0;
+    accessor_1_triangles_vertices.componentType = TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT; // 32 bits per index
+    accessor_1_triangles_vertices.count = M.facets.nb()*3; // how many indices
+    accessor_1_triangles_vertices.type = TINYGLTF_TYPE_SCALAR;
+    // range of indices value : [ 0 : #vertices-1 ]
+    accessor_1_triangles_vertices.maxValues.push_back(M.vertices.nb()-1); // max value of all indices in the buffer view
+    accessor_1_triangles_vertices.minValues.push_back(0);
 
     // layout description of buffer view 2
     accessor_2_edges_vertices.bufferView = BUFFERVIEW_2_EDGES_VERTICES;
@@ -181,7 +180,7 @@ void write_glTF__triangle_mesh(std::string filename, GEO::Mesh& M) {
     accessor_2_edges_vertices.componentType = TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT; // 32 bits per index
     accessor_2_edges_vertices.count = edges.size()*2; // how many indices
     accessor_2_edges_vertices.type = TINYGLTF_TYPE_SCALAR;
-    // range of indices : [ 0 : 1 ]
+    // range of indices value : [ 0 : #vertices-1 ]
     accessor_2_edges_vertices.maxValues.push_back(M.vertices.nb()-1); // max value of all indices in the buffer view
     accessor_2_edges_vertices.minValues.push_back(0);
 
@@ -199,13 +198,13 @@ void write_glTF__triangle_mesh(std::string filename, GEO::Mesh& M) {
     tinygltf::Primitive& primitive_0_triangles = mesh_0.primitives[PRIMITIVE_0_TRIANGLES];
     tinygltf::Primitive& primitive_1_edges = mesh_0.primitives[PRIMITIVE_1_EDGES];
 
-    primitive_0_triangles.indices = ACCESSOR_0_TRIANGLES_VERTICES;
-    primitive_0_triangles.attributes["POSITION"] = ACCESSOR_1_VERTICES_COORDINATES;
+    primitive_0_triangles.indices = ACCESSOR_1_TRIANGLES_VERTICES;
+    primitive_0_triangles.attributes["POSITION"] = ACCESSOR_0_VERTICES_COORDINATES;
     primitive_0_triangles.material = MATERIAL_0_GREY;
     primitive_0_triangles.mode = TINYGLTF_MODE_TRIANGLES;
 
     primitive_1_edges.indices = ACCESSOR_2_EDGES_VERTICES;
-    primitive_1_edges.attributes["POSITION"] = ACCESSOR_1_VERTICES_COORDINATES;
+    primitive_1_edges.attributes["POSITION"] = ACCESSOR_0_VERTICES_COORDINATES;
     primitive_1_edges.material = MATERIAL_1_BLACK;
     primitive_1_edges.mode = TINYGLTF_MODE_LINE;
 
